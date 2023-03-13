67b5416c1a86b30276b8249f851a6172497cb1ef
delomar <romap_3@hotmail.com>
2023-03-13 18:33:17 -0500

    solucion a los ejercicios de js

---
+++ js-2/ejercicios.js
@@ -0,0 +1,49 @@
+function sum(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
+
+function max(arr) {
+    if (arr.length === 0) {
+        return undefined;
+    }
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+function maxIndex(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+    let max = arr[0];
+    let maxIndex = 0;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+
+function join(arr) {
+    let str = "";
+    for (let i = 0; i < arr.length; i++) {
+        str += arr[i];
+        if (i < arr.length - 1) {
+            str += " ";
+        }
+    }
+    return str;
+}
+
+//console.log(maxIndex([1, 3, 2]));
+console.log(max([10, 9, 8, 7, 6, 5, 4]));
